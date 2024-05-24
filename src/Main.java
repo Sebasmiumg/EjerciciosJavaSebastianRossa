cdc6ea16f2cc11ea0bee7eb2b209f11113c8bdff
Sebasmiumg <jrossac@miumg.edu.gt>
2024-05-23 23:10:20 -0600

    EjerciciosJavaSebastianRossa

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public static void main() {
+    EjerciciosJavaSebastianRossa ejercicios = new EjerciciosJavaSebastianRossa();
+
+    // Llamar a los métodos según se desee
+    // Por ejemplo:
+    ejercicios.determinarSigno();
+    ejercicios.verificarParidad();
+    ejercicios.encontrarMayorDeTres();
+    ejercicios.determinarBisiesto();
+    ejercicios.imprimirNumerosAscendentes();
+    ejercicios.imprimirNumerosHasta();
+    ejercicios.sumarNumerosPositivos();
+    ejercicios.generarFibonacci();
+    ejercicios.imprimirPares();
+    ejercicios.mostrarTablaMultiplicar();
+    ejercicios.sumarNumerosPares();
+    ejercicios.calcularFactorial();
+}
